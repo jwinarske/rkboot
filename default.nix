6ad1d470d131dd6beb99a078b3826b9000c53c2f
Jimmy Brisson <theotherjimmy@gmail.com>
2020-04-08 10:38:17 -0500

    Bulid on x86 as well as aarch64

--- default.nix
+++ default.nix
@@ -1,18 +1,24 @@
 # SPDX-License-Identifier: CC0-1.0
-{pkgs ? import <nixos> {
-  #crossSystem={config="aarch64-unknown-linux-musl";};
-}}: {
-  levinboot = pkgs.clangStdenv.mkDerivation {
+{pkgs ? import <nixos> {}}:
+let
+  host = pkgs;
+  aarch64 =  if builtins.currentSystem == "aarch64-linux"
+    then pkgs
+    else pkgs.pkgsCross.aarch64-multiplatform-musl;
+  inherit (host) lib;
+in
+{
+  levinboot = aarch64.stdenv.mkDerivation {
     pname = "levinboot";
     version = "0.0.1";
-    nativeBuildInputs = [pkgs.ninja pkgs.python3];
+    nativeBuildInputs = [host.ninja host.python3];
     configurePhase = ''
       mkdir build
       cd build
       python3 ../configure.py >build.ninja
     '';
     installPhase = "mkdir -p $out; cp levinboot-usb.bin levinboot.img $out";
-    depsBuildBuild = [pkgs.buildPackages.stdenv.cc];
+    depsBuildBuild = [host.buildPackages.stdenv.cc];
     src = builtins.filterSource
       (path: type:
         type == "directory"
@@ -25,11 +31,11 @@
       )
       ./.;
   };
-  tools = pkgs.stdenv.mkDerivation {
+  tools = host.stdenv.mkDerivation {
     pname = "levinboot-tools";
     version = "0.0.1";
-    buildInputs = [pkgs.libusb1];
-    nativeBuildInputs = [pkgs.pkg-config pkgs.ninja];
+    buildInputs = [host.libusb1];
+    nativeBuildInputs = [host.pkg-config host.ninja];
     installPhase = "mkdir -p $out/bin; cp usbtool idbtool regtool $out/bin";
     src = ./tools;
   };
