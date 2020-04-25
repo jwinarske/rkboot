5e3e7eff24b4d7572397c9a40807229b33664f3f
CrystalGamma <dev@crystalgamma.de>
2020-04-26 00:18:07 +0200

    mark 0.2

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #pragma once
 
 /* must be shorter than 64 bytes */
-#define CONFIG_GREETING "levinboot/0.1\r\n"
+#define CONFIG_GREETING "levinboot/0.2\r\n"
 
 /* base clock 1.5MHz */
 #define CONFIG_UART_CLOCK_DIV 1
