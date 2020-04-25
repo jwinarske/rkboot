d673849b1021775c2329cfd4659875944178babc
CrystalGamma <dev@crystalgamma.de>
2020-04-25 22:43:55 +0200

    add missing file

---
+++ string.c
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: CC0-1.0 */
+#include <defs.h>
+
+size_t strnlen(const char *s, size_t maxlen) {
+	const char *start = s, *end = s + maxlen;
+	while (s < end && *s) {s += 1;}
+	return s - start;
+}
