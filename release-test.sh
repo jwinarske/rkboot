dcf1aacd782e1fcc1b44b31e90a147b9ffdff022
CrystalGamma <dev@crystalgamma.de>
2020-04-28 20:00:30 +0200

    add 3 more test configurations

--- release-test.sh
+++ release-test.sh
@@ -6,7 +6,6 @@ src=`dirname "$0"`
 set -e
 
 echo "Source directory: $src"
-echo $@
 
 if [ -z "$atf" -o -z "$artifacts" ]; then
 	echo "usage: $0 path/to/atf/headers path/to/external/artifacts"
@@ -50,6 +49,37 @@ fi
 if [ -z "$skip" -o "$skip" == "4" ]; then
 	echo "Configuration 4: levinboot + elfloader + kernel"
 	"$src/configure.py" --with-atf-headers "$atf"
-	ninja levinboot-usb.bin elfloader.bin teststage.bin
+	ninja levinboot-usb.bin elfloader.bin
 	testrun --call levinboot-usb.bin --load 100000 elfloader.bin --load 200000 "$artifacts/bl31.elf" --load 500000 "$artifacts/fdt.dtb" --load 680000 "$artifacts/Image" --jump 100000 1000
+fi
+
+if [ -z "$skip" -o "$skip" == "5" ]; then
+	echo "Configuration 5: levinboot + elfloader + teststage, gzip compression"
+	"$src/configure.py" --with-atf-headers "$atf" --elfloader-gzip
+	ninja levinboot-usb.bin elfloader.bin teststage.bin
+	gzip -k teststage.bin
+	trap 'rm blob.fifo' ERR
+	mkfifo blob.fifo
+	dtc -@ "$src/overlay-example.dts" -I dts -O dtb -o - | fdtoverlay -i "$artifacts/fdt.dtb" -o - - | gzip | cat "$artifacts/bl31.gz" - teststage.bin.gz >blob.fifo & testrun --call levinboot-usb.bin --load 100000 elfloader.bin --load 2000000 blob.fifo --jump 100000 1000
+	rm blob.fifo
+	trap '' ERR
+	rm teststage.bin.gz
+fi
+
+if [ -z "$skip" -o "$skip" == "6" ]; then
+	echo "Configuration 6: levinboot + elfloader + kernel, mixed compression"
+	"$src/configure.py" --with-atf-headers "$atf" --elfloader-gzip --elfloader-lz4 --elfloader-zstd
+	ninja levinboot-usb.bin elfloader.bin teststage.bin
+	trap 'rm blob.fifo' ERR
+	mkfifo blob.fifo
+	dtc -@ "$src/overlay-example.dts" -I dts -O dtb -o - | fdtoverlay -i "$artifacts/fdt.dtb" -o - - | lz4 | cat "$artifacts/bl31.gz" - "$artifacts/Image.zst" >blob.fifo & testrun --call levinboot-usb.bin --load 100000 elfloader.bin --load 2000000 blob.fifo --jump 100000 1000
+	rm blob.fifo
+	trap '' ERR
+fi
+
+if [ -z "$skip" -o "$skip" == "7" ]; then
+	echo "Configuration 7: levinboot SD image with zstd decompressor"
+	"$src/configure.py" --with-atf-headers "$atf" --embed-elfloader --elfloader-spi --elfloader-zstd
+	ninja levinboot.img
+	echo "Build successful"
 fi
