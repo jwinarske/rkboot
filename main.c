0f8fc3faaa7237dfcc39350cfb49682351997948
CrystalGamma <dev@crystalgamma.de>
2020-06-26 15:37:25 +0200

    remove SPI probing from main

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <uart.h>
 #include <rk3399.h>
 #include <stage.h>
-#include <rkspi_regs.h>
 #include <compression.h>
 
 static const struct mapping initial_mappings[] = {
@@ -48,23 +47,6 @@ int32_t ENTRY NO_ASAN main() {
 	setup_pll(cru + CRU_LPLL_CON, 1200);
 	ddrinit();
 	cru[CRU_CLKSEL_CON+59] = SET_BITS16(1, 1) << 15 | SET_BITS16(7, 3) << 8;
-	const u32 spi_mode_base = SPI_MASTER | SPI_CSM_KEEP_LOW | SPI_SSD_FULL_CYCLE | SPI_LITTLE_ENDIAN | SPI_MSB_FIRST | SPI_POLARITY(1) | SPI_PHASE(1) | SPI_DFS_8BIT;
-	assert((spi_mode_base | SPI_BHT_APB_8BIT) == 0x24c1);
-	volatile struct rkspi *spi = spi1;
-	spi->ctrl0 = spi_mode_base | SPI_BHT_APB_8BIT;
-	spi->enable = 1; mmio_barrier();
-	spi->slave_enable = 1; mmio_barrier();
-	spi->tx = 0x9f;
-	for_range(i, 0, 3) {spi->tx = 0xff;}
-	u32 val = 0;
-	for_range(i, 0, 4) {
-		while (!spi->rx_fifo_level) {__asm__ volatile("yield");}
-		val = val << 8 | spi->rx;
-	}
-	spi->slave_enable = 0; mmio_barrier();
-	spi->enable = 0;
-	printf("SPI read %x\n", val);
-	assert(val != ~(u32)0);
 #ifdef CONFIG_EMBED_ELFLOADER
 	void *loadaddr = (void *)0x4000000;
 	mmu_unmap_range(0, 0xf7ffffff);
