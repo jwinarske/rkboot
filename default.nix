f1f79b9cd9aa114778724e7ac21c7a87211efa0b
CrystalGamma <dev@crystalgamma.de>
2020-06-26 16:28:42 +0200

    update version number to 0.5

--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ in
 {
   levinboot = aarch64.stdenv.mkDerivation {
     pname = "levinboot";
-    version = "0.4";
+    version = "0.5";
     nativeBuildInputs = [host.ninja host.python3 host.lz4];
     configurePhase = ''
       mkdir build
