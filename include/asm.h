ec60b705200598f81af125ea82910b8b9667ed0b
CrystalGamma <dev@crystalgamma.de>
2020-08-12 18:58:16 +0200

    asm: include CFI pseudo-ops in procedure start/end macros

--- include/asm.h
+++ include/asm.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: CC0-1.0 */
 #pragma once
-#define SPROC(name, alignment) .align alignment; name:
-#define PROC(name, alignment) .global name;SPROC(name, alignment)
-#define ENDFUNC(name) .type name, %function;.size name, .-name
+#define SPROC(name, alignment) .align alignment; name:.cfi_startproc
+#define PROC(name, alignment) .global name;SPROC(name, alignment);
+#define ENDFUNC(name) .cfi_endproc;.type name, %function;.size name, .-name
+#define ENDFUNC_NESTED(name) .type name, %function;.size name, .-name
