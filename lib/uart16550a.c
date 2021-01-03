6478addee104c00971bccb30a6df026ab0e31474
CrystalGamma <dev@crystalgamma.de>
2021-01-03 22:30:09 +0100

    uart16550a: enable proper locking
    Now that all C code runs with MMU enabled, we can count on atomics working.

--- lib/uart16550a.c
+++ lib/uart16550a.c
@@ -8,10 +8,10 @@
 HEADER_FUNC uint32_t mmio_r32(volatile uint32_t *reg) {return *reg;}
 HEADER_FUNC void mmio_w32(volatile uint32_t *reg, uint32_t val) {*reg = val;}
 
-//static irq_lock_t console_lock = IRQ_LOCK_INIT;
+static irq_lock_t console_lock = IRQ_LOCK_INIT;
 
 void plat_write_console(const char *str, size_t len) {
-	irq_save_t irq = irq_save_mask();//irq_lock(&console_lock);
+	irq_save_t irq = irq_lock(&console_lock);
 	static const size_t depth = CONFIG_CONSOLE_FIFO_DEPTH;
 	while (len) {
 		size_t this_round = len <= depth ? len : depth;
@@ -23,17 +23,15 @@ void plat_write_console(const char *str, size_t len) {
 			mmio_w32(&console_uart->tx, *str++);
 		}
 	}
-	irq_restore(irq);
-	//irq_unlock(&console_lock, irq);
+	irq_unlock(&console_lock, irq);
 }
 
 int fflush(FILE UNUSED *f) {
-	irq_save_t irq = irq_save_mask();//irq_lock(&console_lock);
+	irq_save_t irq = irq_lock(&console_lock);
 	while (mmio_r32(&console_uart->tx_level)) {
 		arch_relax_cpu();
 	}
-	irq_restore(irq);
-	//irq_unlock(&console_lock, irq);
+	irq_unlock(&console_lock, irq);
 	return 0;
 }
 
