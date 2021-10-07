9c8a9b040796c71921060d64aa33a7695740d531
CrystalGamma <dev@crystalgamma.de>
2021-10-07 17:20:30 +0200

    std: do not use __builtin_strnlen (does not work on clang)

--- include/std/string.h
+++ include/std/string.h
@@ -1,5 +1,10 @@
 /* SPDX-License-Identifier: CC0-1.0 */
 #pragma once
+#include <stdint.h>
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n);
+void *memset(void *dest, int c, size_t n);
 #define memcpy(d, s, n) __builtin_memcpy((d), (s), (n))
 #define memset(d, v, n) __builtin_memset((d), (v), (n))
-#define strnlen(s, n) __builtin_strnlen((s), (n))
+
+size_t strnlen(const char *s, size_t maxlen);
