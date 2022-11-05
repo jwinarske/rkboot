afef8eed14afc02d9ceee052d87392634dfc1d29
CrystalGamma <dev@crystalgamma.de>
2022-11-05 16:20:33 +0100

    loader: send status phase for last BROM transfer
    With this, the host considers the control transfer complete, so lbusb can start
    using the bulk protocol.
    We put the VA of the uncached page into X26.
    
    Signed-off-by: Jona Stubbe <dev@jona-stubbe.de>

--- rk3399/include/rk3399/loader.h
+++ rk3399/include/rk3399/loader.h
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: CC0-1.0
 #pragma once
 
+#define LOADER_PA_UART2 0xff1a0000
+#define LOADER_PA_UNCACHED 0xff8eb000
+
 #define ENUM_LOADER_MAPPINGS(X)\
-	X(uart2, 0xff1a0000, MAP_DEV)\
+	X(uart2, LOADER_PA_UART2, MAP_DEV)\
 	X(emmc, 0xfe330000, MAP_DEV)\
 	X(sdmmc, 0xfe320000, MAP_DEV)\
 	X(spi1, 0xff1d0000, MAP_DEV)\
 	X(dmac1, 0xff6f0000, MAP_DEV)\
-	X(loader_uncached, 0xff8eb000, MAP_UNCACHED)\
+	X(loader_uncached, LOADER_PA_UNCACHED, MAP_UNCACHED)\
 	X(stack, 0xff8e9000, MAP_RW)\
 	X(brom_stack, 0xff8c1000, MAP_RO)	/* unmapped later */\
 	X(brom_data, 0xff8c0000, MAP_RO)	/* unmapped later */\
@@ -23,8 +26,6 @@
 #define LOADER_VA_LOADER_UNCACHED 0xff9f9000
 #define LOADER_VA_BROM_DATA 0xff9f6000
 #define LOADER_VA_OTG0 0xff9f4000
-
-#define LOADER_DWC3_GEVNT 0x400
 
 #ifndef __ASSEMBLER__
 #include <stdint.h>
