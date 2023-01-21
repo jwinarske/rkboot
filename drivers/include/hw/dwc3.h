80a2cf224fec414ce9f4bb8d26873d7c749a7db4
CrystalGamma <dev@crystalgamma.de>
2023-01-21 22:08:01 +0100

    drivers/dwc3: document HW header better
    Signed-off-by: Jona Stubbe <dev@jona-stubbe.de>

--- drivers/include/hw/dwc3.h
+++ drivers/include/hw/dwc3.h
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: CC0-1.0
 #pragma once
 
-// register offsets
+//=== register offsets
 #define DWC3_GEVNT 0x400
 #define DWC3_DCFG 0x700
 #define DWC3_DALEPENA 0x720
 #define DWC3_DEPCMD 0x800
 
-// register bits
+//=== register bits
+//--- DEPCMD
 #define DWC3_DEPCMD_SET_EP_CFG 1
 #define DWC3_DEPCMD_SET_XFER_RSC_CFG 2
 #define DWC3_DEPCMD_GET_ST 3
@@ -19,37 +20,51 @@
 #define DWC3_DEPCMD_IOC 0x100
 #define DWC3_DEPCMD_ACT 0x400
 
+//=== DEPCMD_SET_EP_CFG parameters
+// fifonum is only used only for IN endpoints, for which it has to be
+// unique and in the first 16, even if the hardware has more TXFIFOs
 #define DWC3_DEPCFG0(mps, fifonum, burstsize, seq_num)\
 	(((mps) << 3 & 0x3ff8)\
 	| ((fifonum) << 17 & 0x003e0000)\
 	| ((burstsize) << 22 & 0x03c00000)\
 	| ((seq_num) << 26 & 0x3c000000))
+//--- type field [1..3]
 #define DWC3_DEPCFG0_CTRL_EP 0
 #define DWC3_DEPCFG0_ISOC_EP 2
 #define DWC3_DEPCFG0_BULK_EP 4
 #define DWC3_DEPCFG0_INTR_EP 6
+//---- action field [30..]
 #define DWC3_DEPCFG0_INIT 0
 #define DWC3_DEPCFG0_RESTORE 0x40000000
 #define DWC3_DEPCFG0_MODIFY 0x80000000
+
 #define DWC3_DEPCFG1(intnum, interval, epnum, in)\
 	((intnum) & 0x1f\
 	| ((interval) << 16 & 0x00ff0000)\
 	| ((epnum) << 26 & 0x3c000000)\
 	| ((in) << 25 & 0x02000000))
+//--- interrupt enables
 #define DWC3_DEPCFG1_INTEN_XFER_COMPLETE 0x100
 #define DWC3_DEPCFG1_INTEN_XFER_PROGRESS 0x200
+// not ready status: mostly useful for isoc EPs, since it provides the
+// frame number. Linux used to use it for control EPs as well, but
+// that apparently caused problems.
 #define DWC3_DEPCFG1_INTEN_XFER_NRDY 0x400
 #define DWC3_DEPCFG1_INTEN_FIFO_ERR 0x800
 #define DWC3_DEPCFG1_INTEN_STREAM_EVT 0x2000
+//--- flags
 #define DWC3_DEPCFG1_STREAM_CAP 0x01000000
 #define DWC3_DEPCFG1_BULK_BASED 0x40000000
 #define DWC3_DEPCFG1_FIFO_BASED 0x80000000
 
-// device-mode TRB bits
-#define DWC3_DTRB_HWO 1
+//=== device-mode TRB bits
+#define DWC3_DTRB_HWO 1	//< hardware-owned
 #define DWC3_DTRB_LAST 2
 #define DWC3_DTRB_CHAIN 4
+// Continue on Short Packet: Linux sets this on all non-stream EPs
+// as well as last TRBs for stream EPs.
 #define DWC3_DTRB_CSP 8
+//--- type field [4..8]
 #define DWC3_DTRB_NORMAL 0x10
 #define DWC3_DTRB_SETUP 0x20
 #define DWC3_DTRB_STATUS2 0x30
@@ -58,10 +73,11 @@
 #define DWC3_DTRB_ISOC_FIRST 0x60
 #define DWC3_DTRB_ISOC 0x70
 #define DWC3_DTRB_LINK 0x80
+//--- interrupt flags
 #define DWC3_DTRB_ISP 0x400
 #define DWC3_DTRB_IOC 0x800
 
-// Events format:
+//=== Events format:
 // [0] 0: EP event, 1: device event
 // For EP events:
 // [1] 0: OUT, 1: IN
@@ -74,6 +90,7 @@
 // 6: stream
 // 7: command completion
 // [12..16] Status bits
+// For device events: TODO
 #define DWC3_DEPEVT_IN 2
 #define DWC3_DEPEVT(ep) ((ep) << 2 & 0x3c)
 #define DWC3_DEPEVT_XFER_COMPLETE 0x40
